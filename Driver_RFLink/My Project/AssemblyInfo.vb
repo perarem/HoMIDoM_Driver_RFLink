9f23ebf8d2eaf0cb63f5efcb5be7bff3a8647857
perarem <pierre.clainchard@neuf.fr>
2016-06-01 23:42:21 +0200

    Mise à jour mineure

--- Driver_RFLink/My Project/AssemblyInfo.vb
+++ Driver_RFLink/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("HoMIDoM")> 
 <Assembly: AssemblyProduct("Driver_RFLink")> 
-<Assembly: AssemblyCopyright("Copyright © HoMIDoM 2015")> 
+<Assembly: AssemblyCopyright("Copyright © HoMIDoM 2016")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.3.0.0")> 
+<Assembly: AssemblyFileVersion("1.3.0.0")> 
